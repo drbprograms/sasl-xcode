e8d92663d8f51eba304ab6d3f24502ef7fe1602f
drbprograms <dbrownbridge@me.com>
2021-03-01 11:44:11 +0000

    2021 algorithm update - it works
    added set_hd set_tl etc to enforce atom/hdtl distinction of types

--- sasl/zone.h
+++ sasl/zone.h
@@ -6,8 +6,15 @@ extern void zone_weaken(pointer *pp);
 extern void zone_strengthen(pointer *pp);
 extern void zone_erase(pointer *pp);
 
+extern pointer new_atom(tag t);
+extern pointer new_hdtl(tag t, pointer hd, pointer tl);
+extern pointer change_tag(pointer p, tag t);
 
-extern pointer new_node(tag t);
+extern pointer zone_update(pointer *pp, pointer new);
+
+extern pointer set_hd(pointer p, pointer hd);
+extern pointer set_tl(pointer p, pointer tl);
+
 extern char *err_tag_name(tag t);
 extern void free_node(pointer p);
 
@@ -15,8 +22,7 @@ extern void zone_log_report(FILE *where);
 extern char *zone_pointer_info(pointer p);
 extern int zone_check(void);
 
+void zone_delete2021(pointer p);
 
 unsigned check_set_inuse(pointer n);
-unsigned free_2021algorithm(pointer *pp);
-unsigned set_inuse_2021algorithm(pointer *pp);
 extern unsigned zone_check_deletion(pointer n);
